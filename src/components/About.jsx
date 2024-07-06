76ef7b01ed29993ff25a5ac9af10caa784156b97
ARTAB MAJI <artab.maji1993@gmail.com>
2024-07-06 08:34:13 +0530

    src folder uploaded

---
+++ src/components/About.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+// Import your photo
+import myPhoto from "/Users/ARTAB MAJI/OneDrive/Desktop/Accenture_Web/src/assets/myPhoto.jpg";
+
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Get To Know</p>
+        <h2 className={styles.sectionHeadText}>About Me</h2>
+      </motion.div>
+      
+      <div className='mt-4 grid grid-cols-1 md:grid-cols-3 gap-10'>
+        <div className='md:col-span-1 flex justify-center'>
+          <motion.img
+            src={myPhoto}
+            alt='Artab Maji'
+            className='w-80 h-80 object-cover'
+          />
+        </div>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='md:col-span-2 text-secondary text-[17px] leading-[30px]'
+        >
+          I'm Artab Maji, a passionate software engineer and web developer based in West Bengal. With a strong belief in the transformative power of technology, I've been involved in various projects, creating interactive websites, and efficient software solutions. Problem-solving drives me, and I enjoy tackling challenges with creativity. Beyond work, I'm an avid reader, guitarist, and nature enthusiast. Collaborating with diverse teams is essential to me as it fosters growth and creativity. My goal is to contribute to meaningful projects that positively impact society. I embrace new opportunities and am excited about the endless possibilities in the tech world. Let's connect and make a difference together!
+        </motion.p>
+      </div>
+
+      <div className='mt-4 grid grid-cols-1 md:grid-cols-3 gap-1 md:gap-28 w-full'>
+  {/* Education Section */}
+  <motion.div className='mt-10 md:mt-[1rem]' variants={textVariant()}>
+    <h3 className='text-white text-[24px] font-bold'>Education</h3>
+    <div className='mt-6 text-secondary text-[17px] leading-[30px]'>
+      <p>B.Tech in IT</p>
+      <p>Higher Secondary Education</p>
+    </div>
+  </motion.div>
+
+  <motion.div className='mt-10 md:mt-[1rem]' variants={textVariant()}>
+    <h3 className='text-white text-[24px] font-bold'>Institution</h3>
+    <div className='mt-6 text-secondary text-[17px] leading-[30px]'>
+      <p>Narula Institute of Technology</p>
+      <p>Dhatrigram High School</p>
+    </div>
+  </motion.div>
+
+  <motion.div className='mt-10 md:mt-[1rem]' variants={textVariant()}>
+    <h3 className='text-white text-[24px] font-bold'>G.P.A</h3>
+    <div className='mt-6 text-secondary text-[17px] leading-[30px]'>
+      <p>8.21</p>
+      <p>84.6 %</p>
+    </div>
+  </motion.div>
+</div>
+
+      {/* Passion Section */}
+      <motion.div className='mt-20' variants={textVariant()}>
+        <h3 className='mt-[-3rem] text-white text-[24px] font-bold'>Passion</h3>
+        <div className='mt-5 text-secondary text-[17px] leading-[30px]'>
+          <p>Photography, Recitation.</p>
+        </div>
+      </motion.div>
+      
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,223 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  C,
+  Cplusplus,
+  mySQL,
+  bootstrap,
+  Python,
+  Java,
+  // typescript,
+  html,
+  css,
+  reactjs,
+  // redux,
+  tailwind,
+  // nodejs,
+  // mongodb,
+  git,
+  figma,
+  // docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  breeze,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "Skills",
+    title: "Skills",
+  },
+  {
+    id: "My_services",
+    title: "Services",
+  },
+  {
+    id: "Works",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "UI/UX Design",
+    icon: mobile,
+  },
+  {
+    title: "Photographer",
+    icon: backend,
+  },
+  {
+    title: "App Designer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "C",
+    icon: C,
+  },
+  {
+    name: "C++",
+    icon: Cplusplus,
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrap,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Python",
+    icon: Python,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "mySQL",
+    icon: mySQL,
+  },
+  {
+    name: "Java",
+    icon: Java,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+];
+
+const projects = [
+  {
+    name: "Weather Web App",
+    description:
+      "Web-based App where user can check there live weather report and also 3 days weather forecast. Made using Weather API and Geolocation API. ",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "JS",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Weather API",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/Artab07?tab=repositories",
+  },
+  {
+    name: "Event Countdown Timer",
+    description:
+      "A website where users and companies can put any event countdown timer and also can register themselves through it. ",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "JS",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "BOOTSTRAP",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/Artab07/CodeClauseInternship_COUNTDOWN-TIMER",
+  },
+  {
+    name: "Face Recognition Based Attendance System",
+    description:
+      "An attendance sytem using camera of any institution, which provides seamless entry and attendance tracking.",
+    tags: [
+      {
+        name: "OpenCV",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "green-text-gradient",
+      },
+      {
+        name: "HTML/CSS/JS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/Artab07?tab=repositories",
+  },
+  {
+    name: "Breeze The Only Meditation App You Need",
+    description:
+      "It's a meditation app, whre you can breeze out your stress following a healthy lifestyle with some meditation.",
+    tags: [
+      {
+        name: "Figma",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: breeze,
+    source_code_link: "https://www.figma.com/design/N7kEBUmIWEutw3K1WXllme/Breeze-Only-Meditation-App-You-Need?node-id=1-3&t=NTFVHPP90jdyMcqm-1",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
